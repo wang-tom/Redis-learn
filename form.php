d419f935d1c1aa512af341c64167fa15594d92d5
wang-tom <wanggang20120401@gmail.com>
2015-04-09 18:05:32 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,8 @@
+<form action="reg.php" method="post">
+	用户：<input type="text" name="username" /><br>
+	密码：<input type="password" name="password" /><br>
+	年龄：<input type="text" name="age" /><br>
+	<input type="submit" value="提交" />
+	<input type="reset" value="重置" /> 
+</form>
+
